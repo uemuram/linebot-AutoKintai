9d990a5d144b6acf49e51c0e654ca32e5d5125c2
uemuram <uemura.masaru1124@gmail.com>
2025-06-27 07:04:29 +0900

    コメント除去

--- lambda/chronusUtil.mjs
+++ lambda/chronusUtil.mjs
@@ -216,22 +216,3 @@ function calculateWorkingTime(startTime, endTime) {
 
     return result;
 }
-
-// export async function testYahooAccess() {
-//     const browser = await puppeteer.launch({
-//         args: chromium.args,
-//         defaultViewport: chromium.defaultViewport,
-//         executablePath: await chromium.executablePath(),
-//         headless: chromium.headless,
-//     });
-
-//     let page = await browser.newPage();
-//     await page.goto('https://www.yahoo.co.jp/');
-//     const pageTitle = await page.title();
-
-//     await page.waitForSelector('#tabpanelTopics1');
-//     const innerText = await page.evaluate(() => (document.querySelector('#tabpanelTopics1')).innerText);
-
-//     await browser.close();
-//     return pageTitle + "\n\n" + innerText;
-// }
